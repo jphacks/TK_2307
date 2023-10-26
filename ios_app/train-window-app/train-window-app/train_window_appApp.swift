ee7da298f15a61a04bbb67e5661766f9252952e5
yuta mochizuki <mochi.2225.it@gmail.com>
2023-10-26 20:47:03 +0900

    feat: 新規プロジェクトを作成

---
+++ ios_app/train-window-app/train-window-app/train_window_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  train_window_appApp.swift
+//  train-window-app
+//
+//  Created by yuuta on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct train_window_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
