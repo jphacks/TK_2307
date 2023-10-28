d5589c7f9356862fe5e2dc365da44231f42dc36e
rare0b <rare0b0110@gmail.com>
2023-10-28 22:09:43 +0900

    feat: Spot作成と近い駅のSpot一覧取得

---
+++ server/functions/src/model/spot/SpotModel.ts
@@ -0,0 +1,10 @@
+export interface SpotModel {
+  spotDocumentId: string;
+  stationDocumentId: string;
+  latitude: number;
+  longitude: number;
+  season: number;
+  history: number;
+  time: number;
+  userId: string;
+}
