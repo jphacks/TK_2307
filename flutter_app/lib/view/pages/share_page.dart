8a58eb3f1cf1805c5cd3268e6e8ec8747576362a
mochi-yu <mochi.2225.it@gmail.com>
2023-10-28 12:13:16 +0900

    feat: 通信用のライブラリを追加

--- flutter_app/lib/view/pages/share_page.dart
+++ flutter_app/lib/view/pages/share_page.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import '../../util/http_client.dart';
 
 class SharePage extends StatelessWidget {
   const SharePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("here is a SharePage."),
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () async {
+                final res = await execGetRequest("/getPing");
+                print(res.body);
+              },
+              child: const Text("GET")
+            ),
+            TextButton(
+              onPressed: () async {
+                final res = await execPostRequestWithParam("/createPing", {'ping': 'pong'});
+                print(res.body);
+              },
+              child: const Text("POST")
+            ),            
+          ],
+        )
       ),
     );
   }
