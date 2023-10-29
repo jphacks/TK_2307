ea6098c9c4b25c453dc9803fd0295d466c3f24b5
KanaFujitani <cattleya.leya0@gmail.com>
2023-10-29 11:02:51 +0900

    fix: loginpageのデザイン修正

--- flutter_app/lib/view/pages/login_page.dart
+++ flutter_app/lib/view/pages/login_page.dart
@@ -14,84 +14,135 @@ class _LoginPageState extends State<LoginPage> {
   // 入力したメールアドレス・パスワード
   String _email = '';
   String _password = '';
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Background Location Service'),
-        ),
         body: Center(
-          child: Column(
-            children: [
-              // 1行目 メールアドレス入力用テキストフィールド
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'メールアドレス'),
-                onChanged: (String value) {
-                  setState(() {
-                    _email = value;
-                  });
-                },
-              ),
-              // 2行目 パスワード入力用テキストフィールド
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'パスワード'),
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    _password = value;
-                  });
-                },
-              ),
-              // 3行目 ユーザ登録ボタン
-              ElevatedButton(
-                child: const Text('ユーザ登録'),
-                onPressed: () async {
-                  try {
-                    final User? user = (await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: _email, password: _password))
-                        .user;
-                    if (user != null)
-                      print("ユーザ登録しました ${user.email} , ${user.uid}");
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-              // 4行目 ログインボタン
-              ElevatedButton(
-                child: const Text('ログイン'),
-                onPressed: () async {
-                  try {
-                    // メール/パスワードでログイン
-                    final User? user = (await FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
-                                email: _email, password: _password))
-                        .user;
-                    if (user != null){
-                      print("ログインしました　${user.email} , ${user.uid}");
-                      widget.loginHandler();
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Icon(Icons.person, size: 100, color: Color(0xFF131111)),
+                SizedBox(height: 10),
+                // 1行目 メールアドレス入力用テキストフィールド
+                Container(
+                  width: 250, // テキストフィールドの幅を指定
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'メールアドレス',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.green, // アクティブ時のボーダーカラーを設定
+                        ),
+                      ),
+                    ),
+                    cursorColor: Colors.green,
+                    onChanged: (String value) {
+                      setState(() {
+                        _email = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                // 2行目 パスワード入力用テキストフィールド
+                Container(
+                  width: 250, // テキストフィールドの幅を指定
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'パスワード',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.green, // アクティブ時のボーダーカラーを設定
+                        ),
+                      ),
+                    ),
+                    obscureText: true,
+                    onChanged: (String value) {
+                      setState(() {
+                        _password = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(height: 40),
 
-              ElevatedButton(
-                onPressed: _onCheck,
-                child: Text("Check.")
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                  widget.loginHandler();
-                },
-                child: Text("Logout")
-              )
-            ],
+                // 3行目 ユーザ登録ボタン
+                ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0), // 角の丸みを指定
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(
+                        Colors.green), // ボタンの背景色を青色に設定
+                    minimumSize: MaterialStateProperty.all(
+                        Size(150, 50)), // ボタンの最小サイズを設定
+                  ),
+                  child: Text(
+                    'ユーザ登録',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold, // テキストの太さを太字に設定
+                      fontSize: 16, // テキストのサイズを指定
+                    ),
+                  ),
+                  onPressed: () async {
+                    try {
+                      final User? user = (await FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                                  email: _email, password: _password))
+                          .user;
+                      if (user != null)
+                        print("ユーザ登録しました ${user.email} , ${user.uid}");
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
+                SizedBox(height: 20),
+
+                // 4行目 ログインボタン
+                ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0), // 角の丸みを指定
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(
+                        Colors.green), // ボタンの背景色を緑色に設定
+                    minimumSize: MaterialStateProperty.all(
+                        Size(150, 50)), // ボタンの最小サイズを設定
+                  ),
+                  child: Text(
+                    'ログイン',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold, // テキストの太さを太字に設定
+                      fontSize: 16, // テキストのサイズを指定
+                    ),
+                  ),
+                  onPressed: () async {
+                    try {
+                      // メール/パスワードでログイン
+                      final User? user = (await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: _email, password: _password))
+                          .user;
+                      if (user != null) {
+                        print("ログインしました　${user.email} , ${user.uid}");
+                        widget.loginHandler();
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
